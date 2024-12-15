2e1fe229201c579ec111208c3e55cb10cc012f6e
roky.mlakar@gmail.com <roky.mlakar@gmail.com>
2024-12-15 23:19:58 +0100

    ui demo

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { SidebarDemo } from "./_components/Sidebardemo";
+
+export default function Home() {
+  return (
+    <div>
+      <SidebarDemo />
+    </div>
+  );
+}
